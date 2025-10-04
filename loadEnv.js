45ca381a8665e374619dc9ca9568ffb030240b67
Vand0525 <vand0525@algonquinlive.com>
2025-10-04 11:05:16 -0400

    .

--- loadEnv.js
+++
@@ -1,8 +0,0 @@
-
-import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-dotenv.config({ path: path.resolve(__dirname, '.env') });
